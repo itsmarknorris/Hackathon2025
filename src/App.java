22310541de062333bd05311507d294027c70bf99
itsmarknorris <norrismark520@gmail.com>
2025-03-22 22:03:31 -0700

    Added a ui and updated the directory class' functionality.

---
+++ src/App.java
@@ -0,0 +1,95 @@
+//package com.example;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        ManageDirectories directories = new ManageDirectories();
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+        choiceBox.getItems().addAll("CreateFolder", "ExistingFolder");
+        choiceBox.setValue("CreateFolder");
+
+        TextField textField1 = new TextField();
+        textField1.setPrefWidth(300);
+        textField1.setPromptText("Enter folder name only if creating");
+
+        TextField textField2 = new TextField();
+        textField2.setPrefWidth(300);
+        textField2.setPromptText("Enter folder address only if creating");
+
+        TextField textField3 = new TextField();
+        textField3.setPrefWidth(300);
+        textField3.setPromptText("Enter File name if folder exists");
+
+        TextField textField4 = new TextField();
+        textField4.setPrefWidth(300);
+        textField4.setPromptText("Enter File address if folder exists");
+
+        TextField textField5 = new TextField();
+        textField5.setPrefWidth(300);
+        textField5.setPromptText("Enter address where to add file");
+
+        ChoiceBox<String> choiceBox2 = new ChoiceBox<>();
+        choiceBox2.getItems().addAll("Homework", "Notes", "Pictures");
+
+        Button submitButton = new Button("Submit");
+        submitButton.setOnAction(e -> {
+            String selectedOption = choiceBox.getValue();
+            String text1 = textField1.getText();
+            String text2 = textField2.getText();
+            if (selectedOption.equals("CreateFolder")) {
+                directories.createDirectories(text2, text1);
+            }
+            else if (selectedOption.equals("ExistingFolder")){
+                String text3 = textField3.getText();
+                String text4 = textField4.getText();
+                String text5 = textField5.getText();
+                String text6 = choiceBox2.getValue();
+                directories.moveFile(text4, text3, text5, text6);
+            }
+        });
+        
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(12);
+        grid.setHgap(12);
+
+        grid.add(new Label("Select Option:"), 0, 0);
+        grid.add(choiceBox, 1, 0);
+        grid.add(new Label("Folder Name:"), 0, 1);
+        grid.add(textField1, 1, 1);
+        grid.add(new Label("Destination:"), 0, 2);
+        grid.add(textField2, 1, 2);
+        grid.add(new Label("File Name:"), 0, 3);
+        grid.add(textField3,1,3);
+        grid.add(new Label("File location:"),0,4);
+        grid.add(textField4, 1, 4);
+        grid.add(new Label("Folder:"),0,5);
+        grid.add(textField5,1,5);
+        grid.add(new Label("Select type:"),0,6);
+        grid.add(choiceBox2,1,6);
+        grid.add(submitButton, 1, 7);
+
+        Scene scene = new Scene(grid, 500, 500);
+        stage.setTitle("File Organizer");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
--- src/ManageDirectories.java
+++ src/ManageDirectories.java
@@ -1,47 +1,28 @@
+//package com.example;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.io.File;
-import java.io.FileNotFoundException;
 public class ManageDirectories {
 
 	public void createDirectories(String pathName, String directoryName) {
 		String path = pathName + "\\" + directoryName;
 		File directory = new File(path);
 		directory.mkdir();
-		String imagePath = path + "\\Images";
+		String imagePath = path + "\\Pictures";
 		File imageDirectory = new File(imagePath);
 		imageDirectory.mkdir();
-		String assignmentPath = path + "\\Texts";
-		File assignmentDirectory = new File(assignmentPath);
-		assignmentDirectory.mkdir();
-		String videoPath = path + "\\Videos";
-		File videoDirectory = new File(videoPath);
-		videoDirectory.mkdir();
+		String hwPath = path + "\\Homework";
+		File hwDirectory = new File(hwPath);
+		hwDirectory.mkdir();
+		String notesPath = path + "\\Notes";
+		File notesDirectory = new File(notesPath);
+		notesDirectory.mkdir();
 	}
 	
-	public void moveFile(String originalDirectory, String fileName, String directoryName) {
+	public void moveFile(String originalDirectory, String fileName, String directoryName, String destination) {
 		try {
-			Files.move(Paths.get("originalDirectory + \"\\\\\" + fileName"), Paths.get(selectFolder(directoryName, fileName)));
+			Files.move(Paths.get(originalDirectory + "\\" + fileName), Paths.get(directoryName + "\\" + destination + "\\" + fileName));
 		}
 		catch (Exception e) {return;}
-	}
-	
-	private String selectFolder(String directoryName, String fileName) {
-		try {
-			//String n = Files.probeContentType(Paths.get("C:\\VirtualStudioFiles\\First Pygame.py"));
-			String extension = fileName.substring(fileName.lastIndexOf('.') + 1);
-			if (extension.equals("png")) return directoryName + "\\Images\\" + fileName;
-			if (extension.equals("mov") || extension.equals("MP4")) {
-				return directoryName + "\\Videos\\" + fileName;
-			}
-			if (extension.equals("txt") || extension.equals("csv")) {
-				return directoryName + "\\Texts\\" + fileName;
-			}
-			else return "";
-		}
-		catch (Exception e) { 
-			return "";
-			}
 	}
 }
